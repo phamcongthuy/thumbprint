aa6e47ef1295ecbf8eeb588a4402454be0db9f9a
tofu.am <xuantiep.pham@gmail.com>
2019-03-24 16:31:32 +0700

    Update index.jsx

--- www/src/components/thumbprint-www/hero/index.jsx
+++ www/src/components/thumbprint-www/hero/index.jsx
@@ -3,7 +3,7 @@ import styles from './index.module.scss';
 import heroImg from './heroImg.svg';
 import thumbprintLogo from '../../container/thumbprintLogo.svg';
 
-const description = 'Assets for building high-quality, consistent user experiences at Thumbtack.';
+const description = 'Trang lưu trữ nội dung phát triển mảng trại nuôi thỏ, và nội dung phát triển Node.js của Ngothuy.com.';
 
 const Hero = () => (
     <div className={styles.hero}>
@@ -11,8 +11,8 @@ const Hero = () => (
             <img src={heroImg} className={styles.image} alt="" />
         </div>
         <div className={styles.textBlock}>
-            <img src={thumbprintLogo} className={styles.logo} alt="Thumbprint" />
-            <div className={styles.subhead}>Design System</div>
+            <img src={thumbprintLogo} className={styles.logo} alt="Ngothuy.com Studio Database" />
+            <div className={styles.subhead}>Ngothuy.com Studio Database</div>
             <div className={styles.description}>{description}</div>
         </div>
     </div>
